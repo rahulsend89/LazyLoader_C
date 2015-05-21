e3bb34b71a2519814af11649f627832dd9f279c1
Rahul Malik <ramalik@deloitte.com>
2015-05-21 23:25:53 +0530

    flick fix

--- LazyLoader_C/LazyLoaderLayer.m
+++ LazyLoader_C/LazyLoaderLayer.m
@@ -8,6 +8,7 @@
 
 #import "LazyLoaderLayer.h"
 CGFloat const spaceFromFrame = 1.0f;
+#define DURATION 2.0
 @interface LazyLoaderLayer()
 @end
 
@@ -17,15 +18,29 @@ CGFloat const spaceFromFrame = 1.0f;
 
 - (void)initVariablesWithFrame:(CGRect)frame {
     self.frame = CGRectMake(0, 0, frame.size.width, frame.size.height);
-    self.fillColor = [UIColor greenColor];
-    self.strokeColor = [UIColor redColor];
-    self.strokeWidth = 10.0f;
+    self.fillColor = [UIColor clearColor];
+    self.strokeColor = [UIColor blackColor];
+    self.strokeWidth = frame.size.width/10;
     self.startAngle = 0.0f;
     self.endAngle = 0.0f;
     self.radius = (frame.size.width/2)-(self.strokeWidth/2+1)*2;
-    [self setNeedsDisplay];
+    [self addRotation];
 }
-
+- (id)initWithLayer:(id)layer {
+    if (self = [super initWithLayer:layer]) {
+        if ([layer isKindOfClass:[LazyLoaderLayer class]]) {
+            LazyLoaderLayer *other = (LazyLoaderLayer *)layer;
+            self.startAngle = other.startAngle;
+            self.endAngle = other.endAngle;
+            self.fillColor = other.fillColor;
+            self.radius = other.radius;
+            self.strokeColor = other.strokeColor;
+            self.strokeWidth = other.strokeWidth;
+        }
+    }
+    
+    return self;
+}
 + (BOOL)needsDisplayForKey:(NSString *)key {
     if ([key isEqualToString:@"startAngle"] || [key isEqualToString:@"endAngle"] || [key isEqualToString:@"rotation"]) {
         return YES;
@@ -33,22 +48,54 @@ CGFloat const spaceFromFrame = 1.0f;
     
     return [super needsDisplayForKey:key];
 }
+-(void)addRotation{
+    CABasicAnimation *rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    rotationAnimation.toValue = [NSNumber numberWithFloat: 360.0f*M_PI/180];
+    rotationAnimation.duration = DURATION;
+    rotationAnimation.repeatCount = HUGE_VAL;
+    [self addAnimation:rotationAnimation forKey:@"rotation"];
+    [self startAnimationWithAngle:@"endAngle"];
+}
+
+-(void)startAnimationWithAngle:(NSString*)key{
+    CABasicAnimation* arcAnimation;
+    arcAnimation = [CABasicAnimation animationWithKeyPath:key];
+    arcAnimation.byValue = [NSNumber numberWithFloat:360.0f];
+    arcAnimation.duration = DURATION/2;
+    arcAnimation.cumulative = YES;
+    arcAnimation.additive = YES;
+    arcAnimation.repeatCount = 1;
+    if([key isEqualToString:@"startAngle"]){
+        arcAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
+    }
+    if([key isEqualToString:@"endAngle"]){
+        arcAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+    }
+    //arcAnimation.timingFunction = [CAMediaTimingFunction functionWithControlPoints:0.0:0:0.3:1.0];
+    arcAnimation.delegate = self;
+    [self addAnimation:arcAnimation forKey:key];
+}
+- (void)animationDidStop:(CAPropertyAnimation *)anim finished:(BOOL)flag{
+        if (([[anim keyPath] isEqualToString:@"startAngle"])) {
+            self.endAngle = 0.0;
+            [self startAnimationWithAngle:@"endAngle"];
+        }else{
+            self.endAngle = 0.1f;
+            [self startAnimationWithAngle:@"startAngle"];
+        }
+}
 
 -(void)drawInContext:(CGContextRef)ctx {
     
     CGPoint center = CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2);
-    if(!self.radius)
-        self.radius = (self.frame.size.width/2)-(self.strokeWidth/2+1)*2;
-    CGFloat radius = 44.0;
+    CGFloat radius = self.radius;
     
     CGContextBeginPath(ctx);
     CGContextAddArc(ctx, center.x, center.y, radius, self.startAngle*M_PI/180, self.endAngle*M_PI/180, YES);
-    // Color it
-    CGContextSetFillColorWithColor(ctx, [UIColor clearColor].CGColor);
+    CGContextSetFillColorWithColor(ctx, self.fillColor.CGColor);
     CGContextSetLineCap(ctx,kCGLineCapRound);
-    CGContextSetStrokeColorWithColor(ctx, [UIColor blackColor].CGColor);
-    CGContextSetLineWidth(ctx, 10);
-    
+    CGContextSetStrokeColorWithColor(ctx, self.strokeColor.CGColor);
+    CGContextSetLineWidth(ctx, self.strokeWidth);
     CGContextDrawPath(ctx, kCGPathEOFillStroke);
 }
 @end
--- LazyLoader_C/ViewController.m
+++ LazyLoader_C/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 #import "LazyLoaderLayer.h"
-#define DURATION 2.0
+
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIView *loaderView;
 @property (nonatomic)LazyLoaderLayer *myLoader;
@@ -19,34 +19,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.myLoader = [LazyLoaderLayer layer];
-    [self.myLoader initVariablesWithFrame:self.loaderView.frame];
     [self.loaderView.layer addSublayer:self.myLoader];
-    [self startAnimationWithAngle:@"endAngle"];
-    [self addRotation];
-}
--(void)addRotation{
-    CABasicAnimation *rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
-    rotationAnimation.toValue = [NSNumber numberWithFloat: 360.0f*M_PI/180];
-    rotationAnimation.duration = DURATION;
-    rotationAnimation.repeatCount = HUGE_VAL;
-    [self.myLoader addAnimation:rotationAnimation forKey:@"rotation"];
-}
--(void)startAnimationWithAngle:(NSString*)key{
-    BOOL endPoint = ([[[self.myLoader modelLayer] valueForKey:key] floatValue]>0);
-    NSNumber *toValue = [NSNumber numberWithFloat:endPoint?0.0f:360.0f];
-    
-    CABasicAnimation* arcAnimation;
-    arcAnimation = [CABasicAnimation animationWithKeyPath:key];
-    arcAnimation.toValue = toValue;
-    arcAnimation.duration = DURATION;
-    arcAnimation.cumulative = YES;
-    arcAnimation.repeatCount = 1;
-    arcAnimation.timingFunction = [CAMediaTimingFunction functionWithName:endPoint?kCAMediaTimingFunctionEaseOut:kCAMediaTimingFunctionEaseOut];
-    arcAnimation.delegate = self;
-    [self.myLoader addAnimation:arcAnimation forKey:key];
-}
-- (void)animationDidStop:(CAPropertyAnimation *)anim finished:(BOOL)flag{
-    [self startAnimationWithAngle:([[anim keyPath] isEqualToString:@"endAngle"])?@"startAngle":@"endAngle"];
+    [self.myLoader initVariablesWithFrame:self.loaderView.frame];
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
